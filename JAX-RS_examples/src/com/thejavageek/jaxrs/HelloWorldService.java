4ab2c7eaf1e0e03bf3928ebd27ccc939197dbafe
Fabio <fabio.giammarioli@tiscali.it>
2016-11-02 11:33:08 +0100

    11.33

--- JAX-RS_examples/src/com/thejavageek/jaxrs/HelloWorldService.java
+++ JAX-RS_examples/src/com/thejavageek/jaxrs/HelloWorldService.java
@@ -13,7 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
+
 
 import com.google.gson.Gson;
 
@@ -22,140 +22,56 @@ public class HelloWorldService {
 
 	//private Student st = new Student("Fabio","Giammarioli",27);
 
-
-	@GET
-	@Path("/sayHello")
-	public String sayHello() {
-		return "<h1>Hel World</h1>";
-	}
-
-	@GET
-	@Produces("text/html")
-	@Path("/sayHello2")
-	public String sayHello23() {
-		String s[]= {"22ciao","ciao2","ciao3"};
-		String res="";
-		for(int i=0;i<s.length;i++){
-			res=res+"<br> "+ "<h1><b>"+s[i].toString()+ "</h1></b>";
-		}
-		return res;
-	}
-
-	@POST
-	@Path("/connectDB")
-	@Consumes (MediaType.APPLICATION_JSON)
-	@Produces (MediaType.APPLICATION_JSON)
-	//@Produces (MediaType.TEXT_PLAIN)
-	public Response  JsonToJson(User s){
-
-		//		String username = s.getUsername();
-		//		String password = s.getPassword();
-		//		String email = s.getEmail();
-
-
-		System.out.println("prima di inizializzazione entitymanager OK");
-		EntityManagerFactory  emf = entityManagerUtils.getInstance();
-
-		EntityManager em = emf.createEntityManager();
-
-		System.out.println("dopo di inizializzazione entitymanager OK");
-		em.getTransaction().begin();
-
-		List<User> result = em.createQuery( "from User", User.class ).getResultList();
-
-		for ( User event : result ) {
-			System.out.println( "Event (" + event.getId() + ") : "+" "+ event.getUsername() +" "+  event.getPassword()+" "+ event.getEmail() );
-		}
-
-		em.getTransaction().commit();
-		em.close();
-
-		System.out.println("dopo stammpa elementi OK");
-
-		//	connect();
-
-		//			System.out.println(s.getName());
-		s.setEmail(s.getEmail().toString()+" LAST");
-		Gson gson = new Gson();
-		String resultJ = gson.toJson(s);
-		return Response.status(200).entity(resultJ).build();
-
-	}
-
-
 	@GET
 	@Path("/getusers")
 	@Produces (MediaType.APPLICATION_JSON)
 	public Response  getUser(){
 
-		System.out.println("prima di inizializzazione entitymanager OK");
 		EntityManagerFactory  emf = entityManagerUtils.getInstance();
 		EntityManager em = emf.createEntityManager();
-		System.out.println("dopo di inizializzazione entitymanager OK");
 		em.getTransaction().begin();
 		List<User> result = em.createQuery( "from User", User.class ).getResultList();	
 		em.getTransaction().commit();
 		em.close();
-		System.out.println("dopo stammpa elementi OK");
 		String resultJ = new Gson().toJson(result);
 		return Response.status(200).entity(resultJ).build();
-
 	}
-
-	@GET			////////////////DA COMPLETARE  /////////////////
-	@Path("/searchUser")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public ResponseBuilder checkUser(User u){	
-		return Response.status(200);
-	}/////////////////////////////////////////////////////////////////
 
 
 	@POST
 	@Path("/newUser")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_HTML)
-	public String registerUser(User u){
-
+	@Produces(MediaType.APPLICATION_JSON)
+	public ResponseRequest registerUser(User u){
+		ResponseRequest ResponseQuery = new ResponseRequest(); 
 		EntityManagerFactory  emf = entityManagerUtils.getInstance();
 		EntityManager em = emf.createEntityManager();
-		String usernameFind = u.getUsername();
+		if ((u.getUsername().length()<2) || (u.getPassword().length()<2) || (u.getEmail().length()<2) ){
+			ResponseQuery.setDescription("Uno o più campi vuoti");
+			return ResponseQuery;
+		}
 
-		boolean findOK=false;
+
 		em.getTransaction().begin();
 		List<User> result = em.createQuery( " from User", User.class ).getResultList();
 		for (int i=0;i<result.size();i++){
 
-			if (result.get(i).getUsername().equals(usernameFind))
-				findOK=true;
+			if (result.get(i).getUsername().equals(u.getUsername()) || result.get(i).getEmail().equals(u.getEmail())){
+				ResponseQuery.setDescription("Utente già registrato");
+				em.getTransaction().commit();
+				em.close();	
+				return ResponseQuery;
+			}
 		}
-		//User check = em.find(User.class, u.getUsername());
-		//if(check!=null){
-		if(findOK==true)
-			return "Utente già registrato";
-		//}
+
+
+		u.setPassword(convertMD5.encrypt(u.getPassword()));
 		em.persist(u);	
 		em.getTransaction().commit();
 		em.close();		
-			return "Nuovo Utente inserito";
+		ResponseQuery.setDescription("Utente registrato");
+		return ResponseQuery;
 	}
-	
-	
-
-	@GET
-	@Produces("text/html") 
-	@Path("/convertToMD5")
-	public String convertToMD5(){
-		String convString="stringa";
-		convertMD5 MD5Util = new convertMD5(); 
-
-		return "Normale: "+convString+" --> MD5: "+MD5Util.encrypt(convString);
-
-	}
-
-
-
-
 
 
 }
